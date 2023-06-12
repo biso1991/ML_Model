9171ffab76417b7588a3f2fe1610a8f1bb9d517c
biso1991 <bilelnasrinasri91@gmail.com>
2023-06-12 17:11:48 +0000

    add test with train

--- .devcontainer/x1_get_data.py
+++ .devcontainer/x1_get_data.py
@@ -4,9 +4,9 @@ import zipfile
 
 
 
-# cwd = os.getcwd()  # Get the current working directory (cwd)
-# files = os.listdir(cwd)  # Get all the files in that directory
-# print("Files in %r: %s" % (cwd, files))
+# cwd = os.getcwd()  # Get the current working directory 
+# files = os.listdir  # Get all the files in that directory
+# print("Files in %r: %s" % (cmd, files))
 
 # # Download the zipped dataset
 # url = ''
